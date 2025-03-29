339df690eeaad7d92fdf55006f1dc5db6c7451b6
Cristina <kamuthucristina@gmail.com>
2025-03-29 22:27:02 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+      .then(response => {
+          if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json();
+      })
+      .then(data => renderBooks(data))
+      .catch(error => console.log("Error fetching books:", error));
 }
 
 function renderBooks(books) {
